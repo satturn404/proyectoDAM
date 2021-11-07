85cd99b104c60169e1563cdc9a1ec3c0fcad462e
hilario <tomhb404@gmail.com>
2021-11-07 16:13:36 -0500

    hito 3

--- app/src/main/java/com/HilarioBazanToM/umbrellacorpz/login.java
+++ app/src/main/java/com/HilarioBazanToM/umbrellacorpz/login.java
@@ -1,69 +1,50 @@
 package com.HilarioBazanToM.umbrellacorpz;
-
-import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.lifecycle.ViewModelProvider;
 
-import android.media.MediaPlayer;
+import android.nfc.Tag;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
-import android.widget.Toast;
+import android.widget.EditText;
 
-import com.firebase.ui.auth.AuthUI;
-import com.google.android.gms.auth.api.Auth;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-
-import java.util.Arrays;
-import java.util.List;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
 public class login extends AppCompatActivity {
-    FirebaseAuth mfirebaseAutH;
-    FirebaseAuth.AuthStateListener mAuthListener;
-    public static final int REQUEST_CODE = 54654;
-    List<AuthUI.IdpConfig> provider = Arrays.asList (
-            new AuthUI.IdpConfig.FacebookBuilder().build(),
-            new AuthUI.IdpConfig.GoogleBuilder().build()
-            );
-
+    EditText username, password;
+    FirebaseFirestore db;
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+    protected void onCreate(Bundle savedInstance){
+        super.onCreate(savedInstance);
         setContentView(R.layout.activity_login);
+        username = findViewById(R.id.username);
+        password = findViewById(R.id.password);
+        db = FirebaseFirestore.getInstance();
+    }
 
-        mfirebaseAutH = FirebaseAuth.getInstance();
-        mAuthListener = new FirebaseAuth.AuthStateListener() {
-            @Override
-            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
-                FirebaseUser user = firebaseAuth.getCurrentUser();
-                if(user !=null ){
-                    Toast.makeText( login.this,"Bienvenido Guapo", Toast.LENGTH_SHORT).show();
-                }else {
-                    startActivityForResult (AuthUI.getInstance()
-                        .createSignInIntentBuilder()
-                        .setAvailableProviders(provider)
-                        .setIsSmartLockEnabled(false)
-                        .build(),REQUEST_CODE
-                    );
-                }
-            }
-        };
+    public void iniLogin(View view) {
+        String usuario = username.getText().toString();
+        String contrasenha = password.getText().toString();
+
+        db.collection("users")
+                .whereEqualTo("user",usuario)
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()){
+                            for (QueryDocumentSnapshot document : task.getResult()){
+                                Log.d("Exito",document.getId()+ " => " + document.getData());
+                            }
+                        }else{
+                            Log.d("ERROR","error getting docments: ", task.getException());
+                        }
+                    }
+                });
+
     }
-    @Override
-    protected void onResume(){
-        super.onResume();
-        mfirebaseAutH.addAuthStateListener(mAuthListener);
-    }
-    @Override
-    protected void onPause(){
-        super.onPause();
-        mfirebaseAutH.addAuthStateListener(mAuthListener);
-    }
-    public void cerrarsession(View view) {
-        AuthUI.getInstance().singOut(this).addOnComppleteListener(new OnCompleteListener<Void>(){
-            @Override
-            public void onComplete(@NonNull Task>void> task) {
-        }
-    });
 }
